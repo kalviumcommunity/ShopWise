e0b782e8e5bc355b3f364090ac751ede7ef51994
Ruhani FashionFantasy <ruhanifashionfantasy@Ruhanis-MacBook-Air.local>
2023-07-11 10:14:50 +0530

    Fetch API data using Axios

---
+++ shopwise/src/reducer/productReducer.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const productReducer = (state,action) => {
+    // if(action.type==="SET_LOADING"){
+    //     return{
+    //         ...state,
+    //         isLoading:true
+    //     };
+    // }
+
+    // if(action.type==="API_ERROR"){
+    //     return{
+    //         ...state,
+    //         isLoading:false,
+    //         isError:true
+    //     };
+    // }
+
+    switch(action.type){
+        case "SET_LOADING":
+            return{
+                ...state,
+                isLoading:true
+            };
+
+
+        case "API_ERROR":
+            return {
+                ...state,
+                isLoading:false,
+                isError:true
+            };
+        case "SET_API_DATA":
+            const featureData=action.payload.filter((currElem)=>{
+                return currElem.filter=="true";
+            });
+
+            return {
+                ...state,
+                isLoading:false,
+                products:action.payload,
+                featureProducts:featureData,
+            }
+            
+            
+        default:
+            return state;
+    }
+
+
+  return state;
+}
+
+export default productReducer
