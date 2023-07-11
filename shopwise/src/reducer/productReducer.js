542300800b30779101aaa42730074b7aaadbc844
Ruhani FashionFantasy <ruhanifashionfantasy@Ruhanis-MacBook-Air.local>
2023-07-11 10:34:35 +0530

    comments removed

--- shopwise/src/reducer/productReducer.js
+++ shopwise/src/reducer/productReducer.js
@@ -1,20 +1,6 @@
 import React from 'react'
 
 const productReducer = (state,action) => {
-    // if(action.type==="SET_LOADING"){
-    //     return{
-    //         ...state,
-    //         isLoading:true
-    //     };
-    // }
-
-    // if(action.type==="API_ERROR"){
-    //     return{
-    //         ...state,
-    //         isLoading:false,
-    //         isError:true
-    //     };
-    // }
 
     switch(action.type){
         case "SET_LOADING":
